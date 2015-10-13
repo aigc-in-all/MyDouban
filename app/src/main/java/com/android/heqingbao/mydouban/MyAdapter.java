45016ddc81260028bc2282dc797687252ee69278
HeQingbao <qingbao.ho@gmail.com>
2015-10-13 13:08:32 +0800

    Init project

---
+++ app/src/main/java/com/android/heqingbao/mydouban/MyAdapter.java
@@ -0,0 +1,48 @@
+package com.android.heqingbao.mydouban;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by HeQingbao on 2015/10/12.
+ */
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<String> mDataset;
+
+    public MyAdapter(List<String> myDataset) {
+        mDataset = myDataset;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mDataset.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTextView;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextView = (TextView) itemView.findViewById(R.id.info_text);
+        }
+    }
+}
